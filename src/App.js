4c0a3c2bf8d0775189982478adc66fcce15dc11f
Vincent Romine <vinjrom@live.com>
2019-08-28 23:40:41 -0500

    update images

--- src/App.js
+++ src/App.js
@@ -55,7 +55,7 @@ class App extends Component {
     this.setState({
       currentScore: 0,
       topScore: this.state.topScore,
-      rightWrong: "Glaven!",
+      rightWrong: "Oof. Try again!",
       clicked: []
     });
     this.handleShuffle();
